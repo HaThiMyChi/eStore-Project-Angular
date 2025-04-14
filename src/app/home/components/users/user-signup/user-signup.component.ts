e5db9fed7129668f84ff593eb8b274214e14b668
HaThiMyChi <mychikhtn@gmail.com>
2025-04-14 17:24:36 +0700

    Connect sign up form to API

--- src/app/home/components/users/user-signup/user-signup.component.ts
+++ src/app/home/components/users/user-signup/user-signup.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
 import { matchPasswords } from './validators/match-passwords.validator';
+import { UserService } from 'src/app/home/services/users/user-service.service';
+import { user } from 'src/app/home/types/user.type';
+
 
 @Component({
   selector: 'app-user-signup',
   templateUrl: './user-signup.component.html',
-  styleUrls: ['./user-signup.component.scss']
+  styleUrls: ['./user-signup.component.scss'],
+  providers: [UserService]
 })
 export class UserSignupComponent implements OnInit{
   userSignupForm: FormGroup;
-  constructor(private fb: FormBuilder) {}
+  alertMessage: string = '';
+  alertType: number = 0; // 0-success, 1-warning, 2-error
+  constructor(private fb: FormBuilder, private userService: UserService) {}
 
   ngOnInit(): void {
     this.userSignupForm = this.fb.group({
@@ -19,13 +25,13 @@ export class UserSignupComponent implements OnInit{
       city: [''],
       state: [''],
       pin: [''],
-      email: ['', Validators.required, Validators.email],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required]
     },
     {
       validator: matchPasswords,
-    })
+    });
   }
 
   get firstName() : AbstractControl<any, any> | null {
@@ -45,6 +51,31 @@ export class UserSignupComponent implements OnInit{
   }
 
   onSubmit(): void {
+    const user: user = {
+      firstName: this.firstName?.value,
+      lastName: this.userSignupForm.get('lastName')?.value,
+      address: this.userSignupForm.get('address')?.value,
+      city: this.userSignupForm.get('city')?.value,
+      state: this.userSignupForm.get('state')?.value,
+      pin: this.userSignupForm.get('pin')?.value,
+      email: this.userSignupForm.get('email')?.value,
+      password: this.userSignupForm.get('password')?.value,
+    };
 
+    this.userService.createUser(user).subscribe({
+      next: (result) => {
+        if (result.message === 'success') {
+          this.alertMessage = 'User created successfully.';
+          this.alertType = 0;
+        } else if(result.message === 'Email already exists') {
+          this.alertMessage = result.message;
+          this.alertType = 1;
+        }
+      },
+      error: (error) => {
+        this.alertMessage = error.message;
+        this.alertType = 2;
+      }
+    })
   }
 }
